3e35274dd224ed846f78fbf26e6bc4392f9d2b56
Zebreus <zebreus@zebre.us>
2025-07-08 20:50:21 +0200

    Add mysqlclient test script

---
+++ tests/mysqlclient-test.py
@@ -0,0 +1,11 @@
+# docker run --rm -it --name some-mysql -e MYSQL_ROOT_PASSWORD=password  mysql:latest
+
+from MySQLdb import _mysql
+db=_mysql.connect(host="127.0.0.1",port=3306,user="root",password="password")
+
+db.query("SELECT VERSION()")
+
+result = db.use_result()
+row = result.fetch_row()
+
+print("MySQL Server Version:", row[0][0])
