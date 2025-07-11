03abeca616e23f87aaf369cd64e968005188f79f
Zebreus <zebreus@zebre.us>
2025-07-11 12:27:33 +0200

    Fix port in the suggested docker command in the mysql test

--- tests/mysqlclient-test.py
+++ tests/mysqlclient-test.py
@@ -1,4 +1,4 @@
-# docker run --rm -it --name some-mysql -e MYSQL_ROOT_PASSWORD=password  mysql:latest
+# docker run -p 3306:3306 --rm -it --name some-mysql -e MYSQL_ROOT_PASSWORD=password  mysql:latest
 
 from MySQLdb import _mysql
 db=_mysql.connect(host="127.0.0.1",port=3306,user="root",password="password")
