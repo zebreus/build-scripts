3904a6d5df540b6471db08c9f889673eca269dc2
Zebreus <zebreus@zebre.us>
2025-08-21 17:17:11 +0200

    Add unused cmake toolchain file

---
+++ resources/wasix-toolchain.cmake
@@ -0,0 +1,26 @@
+# Cmake toolchain description file for the Makefile for WASI
+cmake_minimum_required(VERSION 3.5.0)
+
+set(CMAKE_SYSTEM_NAME WASI) # Generic for now, to not trigger a Warning
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR wasm32)
+set(CMAKE_C_COMPILER_ID Clang)
+set(triple wasm32-unknown-wasi)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+set(CMAKE_ASM_COMPILER_TARGET ${triple})
+# set(CMAKE_${lang}_COMPILE_OPTIONS_SYSROOT "--sysroot=$ENV{WASIX_SYSROOT}")
+# set(CMAKE_SYSROOT "$ENV{WASIX_SYSROOT}")
+
+set(CMAKE_C_COMPILER wasix-clang)
+set(CMAKE_CXX_COMPILER wasix-clang++)
+set(CMAKE_LINKER wasm-ld)
+set(CMAKE_AR llvm-ar)
+set(CMAKE_RANLIB llvm-ranlib)
+
+# Don't look in the sysroot for executables to run during the build
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# Only look in the sysroot (not in the host paths) for the rest
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
