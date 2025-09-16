3a4eb93161c657dd7e78cfb765fcafcae19c9655
Zebreus <zebreus@zebre.us>
2025-09-16 11:45:53 +0200

    Set CMAKE_SYSTEM_NAME back to WASI

--- resources/wasix-toolchain.cmake
+++ resources/wasix-toolchain.cmake
@@ -1,9 +1,8 @@
 # Cmake toolchain description file for the Makefile for WASI
 cmake_minimum_required(VERSION 3.5.0)
 
-# Don't set this to WASI as that leaves `UNIX` set to false.
-set(CMAKE_SYSTEM_NAME UnixPaths) # Set to UnixPaths to get a UNIX like environment. We dont care about the other setting set in there.
-set(WASI 1)
+set(CMAKE_SYSTEM_NAME WASI) # compiler-rt does not build the profile rt files if this is not set to WASI.
+set(UNIX 1 CACHE BOOL "" FORCE) # curl only builds on UNIX systems so we force UNIX to be set (CMAKE_SYSTEM_NAME=WASI does not set UNIX)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR wasm32)
 set(CMAKE_C_COMPILER_ID Clang)
