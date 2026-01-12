ebeae3b821fd24ce6f5e54d5f8d516acc8117713
Zebreus <zebreus@zebre.us>
2026-01-12 10:50:45 +0100

    Integrate fixed pyopenssl tests into the main pyopenssl tests

--- tests/pyopenssl-test-broken2.py
+++
@@ -1,4 +0,0 @@
-from cryptography.hazmat.bindings.openssl.binding import Binding
-binding = Binding()
-ffi = binding.ffi
-ffi.callback( "void (*)(const SSL *, const char *)", lambda a : a + 10 )
--- tests/pyopenssl-test.py
+++ tests/pyopenssl-test.py
@@ -571,6 +571,57 @@ class TestPyOpenSSL(unittest.TestCase):
         client.shutdown()
         server.shutdown()
 
+    def test_legacy_pyopenssl_cert_generation(self):
+        """Test the older pyOpenSSL-native key/cert generation (may have deprecation warnings)."""
+        def generate_key(bits=2048):
+            pkey = crypto.PKey()
+            pkey.generate_key(crypto.TYPE_RSA, bits)
+            return pkey
+
+        def generate_self_signed_cert(common_name=b"localhost", days_valid=365, pkey=None, digest=b"sha256"):
+            if pkey is None:
+                pkey = generate_key()
+
+            cert = crypto.X509()
+            subj = cert.get_subject()
+            subj.CN = common_name.decode()
+            subj.O = "Test Org"
+            subj.OU = "Testing"
+            subj.C = "US"
+            cert.set_serial_number(int(time.time()))
+            cert.gmtime_adj_notBefore(0)
+            cert.gmtime_adj_notAfter(days_valid * 24 * 60 * 60)
+            cert.set_issuer(subj)
+            cert.set_pubkey(pkey)
+            cert.set_version(2)  # v3 (0-based)
+            # Add a couple of extensions
+            cert.add_extensions([
+                crypto.X509Extension(b"basicConstraints", False, b"CA:TRUE"),
+                crypto.X509Extension(b"keyUsage", False, b"keyEncipherment, dataEncipherment, digitalSignature, keyCertSign"),
+                crypto.X509Extension(b"subjectAltName", False, b"DNS:localhost,IP:127.0.0.1"),
+            ])
+            cert.sign(pkey, digest.decode())
+            return cert, pkey
+
+        # Test the generation
+        cert, pkey = generate_self_signed_cert(common_name=b"localhost")
+        self.assertIsInstance(cert, crypto.X509)
+        self.assertIsInstance(pkey, crypto.PKey)
+        self.assertEqual(cert.get_subject().CN, "localhost")
+        self.assertEqual(cert.get_issuer().CN, "localhost")
+        self.assertGreaterEqual(cert.get_extension_count(), 3)
+
+    def test_cryptography_ffi_callback(self):
+        """Test that cryptography's FFI binding and callback functionality works."""
+        from cryptography.hazmat.bindings.openssl.binding import Binding
+        binding = Binding()
+        ffi = binding.ffi
+        
+        # Test creating a callback - the lambda should be callable
+        # We use a simple callback signature that doesn't actually get invoked
+        callback = ffi.callback("void (*)(const SSL *, const char *)", lambda ssl_ptr, msg: None)
+        self.assertIsNotNone(callback)
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
